120fbcc34abe83c9b75b983ccf464db931ef412b
Erwin Bofetiado <bofetiad@amazon.com>
2024-12-28 00:20:43 -0600

    removed black

--- lua/custom/config/null-ls.lua
+++ lua/custom/config/null-ls.lua
@@ -3,7 +3,6 @@ null_ls.setup {
   sources = {
     null_ls.builtins.diagnostics.mypy,
     null_ls.builtins.diagnostics.ruff,
-    null_ls.builtins.formatting.black,
     null_ls.builtins.formatting.prettier,
     null_ls.builtins.formatting.clang_format,
   },
