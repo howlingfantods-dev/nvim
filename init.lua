55482028f4fd002be5ff376d5d3fcb84dbe02071
Erwin Bofetiado <bofetiad@amazon.com>
2025-01-03 14:47:07 -0600

    none

--- init.lua
+++ init.lua
@@ -453,7 +453,7 @@ require('lazy').setup({
         clangd = {},
         -- gopls = {},
         rust_analyzer = {},
-        -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
+        pyright = {},
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
         --    https://github.com/pmizio/typescript-tools.nvim
