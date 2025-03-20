98002db0190b4b24c2a7594291f7d81824798da5
ebof1223 <rmf97@txstate.edu>
2025-03-20 14:44:05 -0500

    migrating packages to Mason

--- lua/custom/config/null-ls.lua
+++ lua/custom/config/null-ls.lua
@@ -1,12 +1,7 @@
 local null_ls = require 'null-ls'
 
 local opts = {
-  sources = {
-    null_ls.builtins.diagnostics.mypy,
-    null_ls.builtins.diagnostics.ruff,
-    null_ls.builtins.formatting.prettier,
-    null_ls.builtins.formatting.clang_format,
-  },
+  sources = {},
 }
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
